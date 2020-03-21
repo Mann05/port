6c55fece90f7db3c1c574a163b850c9ac12b1a9a
Mann Aikansh <aikanshmann@gmail.com>
2020-03-21 11:50:01 +0530

    Updates

---
+++ precache-manifest.389d8e0f6fe01d8a6991c3930867e01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e6fc9098ea7a63d48094781f4a38d8",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "5a2337fdf1313f580f77",
+    "url": "/port/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "06fc09688e4b2585e162",
+    "url": "/port/static/js/2.e733b6d9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/port/static/js/2.e733b6d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a2337fdf1313f580f77",
+    "url": "/port/static/js/main.1b193938.chunk.js"
+  },
+  {
+    "revision": "5ad3bb77966f351c6b4d",
+    "url": "/port/static/js/runtime-main.20b148a6.js"
+  }
+]);
