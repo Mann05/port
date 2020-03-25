7285ec56f776fb0fe00817bd15de7e713b66f134
Mann Aikansh <aikanshmann@gmail.com>
2020-03-25 21:06:30 +0530

    some chnages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import React from 'react';
-import {BrowserRouter  as Router,Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
 import './App.css';
-import Header from './Header';
-import AboutUs from './About';
-import Resume from './Resume';
-import Portfolio from './PortFolio';
-import Testimonial from './Testimonial';
-import Footer from './CallToAction';
-import CallToAction from './CallToAction';
-import Contact from './Contact';
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <Header/>
-        <AboutUs/>
-        <Resume/>
-        <Portfolio/>
-        <CallToAction/>
-        <Testimonial/>
-        <Contact/>
-      <Footer/>
-    </div>
-    </Router>
-  );
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
+      </div>
+    );
+  }
 }
 
 export default App;
